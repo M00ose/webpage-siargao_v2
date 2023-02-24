2531473437da1971c7f43500e7e0f52285799499
Admin <tyrone.jacalan@protonmail.com>
2023-02-25 00:11:05 +0800

    Removed unused sections

--- sections/index.js
+++ sections/index.js
@@ -1,13 +1,9 @@
 import Hero from './Hero';
 import About from './About';
 import Recap from './Recap';
-import Events from './Events';
-import Activities from './Activities';
 
 export {
     Hero,
     Recap,
-    Events,
-    Activities,
     About,
 }
