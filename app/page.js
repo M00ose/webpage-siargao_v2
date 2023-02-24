eb58be64fa31b77da3b78a3f32c09ba303ba0dad
Admin <tyrone.jacalan@protonmail.com>
2023-02-25 00:09:08 +0800

    Removed unused pages

--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
-import { Navbar, Footer } from '../components';
-import { Hero, Recap, Events, Activities, About } from '../sections';
+import { Footer } from '../components';
+import { Hero, Recap, About } from '../sections';
 
 import { Passion_One } from '@next/font/google';
 import { Londrina_Solid } from '@next/font/google';
@@ -11,9 +11,7 @@ const LondrinaSolid = Londrina_Solid({ subsets: ['latin'], weight: '100' });
 const page = () => (
   <div className={`bg-brand-orange snap-y	snap-mandatory overflow-hidden overscroll-scroll ${PassionOne.className}`}>
     <Hero />
-    <Recap /> 
-    <Events /> 
-    <Activities /> 
+    {/* <Recap />  */}
     <About /> 
     <Footer />
   </div>
