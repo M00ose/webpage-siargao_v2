07fe12b6c22b544147c146cb75af21bfc129932d
Admin <tyrone.jacalan@protonmail.com>
2023-02-28 00:37:09 +0800

    Added purplewave path

--- app/styles.js
+++ app/styles.js
@@ -17,9 +17,11 @@ const styles = {
     largeText:'md:text-[12rem] sm:text-[12rem] xs:text-9xl text-9xl text-white',
     largeAltText:'md:text-[12rem] sm:text-[12rem] xs:text-9xl text-9xl text-brand-yellow',
     mediumText:'md:text-4xl sm:text-6xl xs:text-4xl text-4xl text-white',
-    smallText:'md:text-5xl sm:text-4xl text-3xl text-white',
+    smallText:'md:text-5xl sm:text-2xl text-3xl text-white',
     smallAltText:'md:text-5xl sm:text-4xl text-3xl text-brand-yellow',
-    extraSmallText:'md:text-1xl sm:text-2xl xs:text-1xl text-1xl text-white',
+    extraSmallText:'md:text-1xl sm:text-xs xs:text-xs text-white',
+
+    purpleWave: "M0,96L30,80C60,64,120,32,180,16C240,0,300,0,360,10.7C420,21,480,43,540,74.7C600,107,660,149,720,181.3C780,213,840,235,900,229.3C960,224,1020,192,1080,186.7C1140,181,1200,203,1260,186.7C1320,171,1380,117,1410,90.7L1440,64L1440,320L1410,320C1380,320,1320,320,1260,320C1200,320,1140,320,1080,320C1020,320,960,320,900,320C840,320,780,320,720,320C660,320,600,320,540,320C480,320,420,320,360,320C300,320,240,320,180,320C120,320,60,320,30,320L0,320Z",
   };
   
   export default styles;
