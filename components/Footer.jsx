62a350564db5ed998765f768cf585501c3530dfc
Admin <tyrone.jacalan@protonmail.com>
2023-03-06 17:20:19 +0800

    Initial commit

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+'use client'
+import Email from '@mui/icons-material/Email';
+import Phone from '@mui/icons-material/Phone';
+
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='bg-brand-purple py-12 px-48 flex flex-col gap-6  '>
+      <div className='bg-white h-[2px] w-[20vw]'></div>
+      <div className='flex flex-row gap-8 text-white text-2xl'>
+        <div className='flex flex-1'>
+          <p>Contact Us for Partnerships and Concerns</p>
+        </div>
+        <div>
+          <a href="mailto:info@siargaoisf.ph"><Email className='hover:scale-125 hover:-translate-x-2'/>info@siargaoisf.ph</a>
+        </div>
+        <div>
+          <p><span><Phone /></span>(+63) 908 986 9873</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
