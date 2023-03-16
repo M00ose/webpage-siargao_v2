98438f9a02a2722b5225ec75c16542be6afa8bde
Admin <tyrone.jacalan@protonmail.com>
2023-03-16 21:57:09 +0800

    Added imports for Footer/Recap

--- components/index.js
+++ components/index.js
@@ -1,9 +1,11 @@
 import Navbar from './Navbar';
 import Socials from './Socials';
+import Logo from './Logo';
 import Footer from './Footer';
 
 export {
     Navbar,
     Socials,
+    Logo,
     Footer,
 }
--- sections/index.js
+++ sections/index.js
@@ -1,7 +1,9 @@
 import Hero from './Hero';
 import About from './About';
+import Recap from './Recap';
 
 export {
     Hero,
     About,
+    Recap,
 }
